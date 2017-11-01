bbba938c0a27e4ebfcf98ea5bd8987630e91eb7d
Nagarjunareddy Marella <Nagarjunareddy_Marella@epam.com>
2017-11-01 17:17:27 +0530

    Worked on review comments for Task1

--- Task1/TransportationOnVacation.js
+++ Task1/TransportationOnVacation.js
@@ -1,11 +1,9 @@
 function rentalCarCost(noOfDays) {
-	var pricePerDay = 40;
-	var discountPrice = 0;
+	var pricePerDay = 40, discountPrice = 0;
 	if (noOfDays >= 7) {
 		discountPrice = 50;
-	}
-	else if (noOfDays >= 3) {
+	} else if (noOfDays >= 3) {
 		discountPrice = 20;
 	}
-	return (noOfDays * pricePerDay - discountPrice);
+	return ((noOfDays * pricePerDay) - discountPrice);
 }
